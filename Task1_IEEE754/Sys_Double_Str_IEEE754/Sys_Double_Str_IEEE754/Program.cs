84e126024809c5bfcdfb5eb985955c78d9a5c90b
Zhenis Duissekov <zduissekov@gmail.com>
2019-10-13 20:07:56 +0600

    GCD including a little bit of tests and not finished Sys double

---
+++ Task1_IEEE754/Sys_Double_Str_IEEE754/Sys_Double_Str_IEEE754/Program.cs
@@ -0,0 +1,96 @@
+﻿// header
+
+
+namespace Sys_Double_Str_IEEE754
+{
+    using System;
+
+    public class Program
+    {
+        public static string decimal2binary(string str)
+        {
+            int a = Convert.ToInt32(str);
+            Console.WriteLine("decimal: {0}", a);
+            int i = 0;
+            string result = "";
+            while (a > 0 && i < 30)
+            {
+                i++;
+                result = Convert.ToString(a % 2) + result;
+                a = a / 2;
+            }
+
+            return result;
+        }
+
+        public static string decimal2binary1(string str)
+        {
+            int a = Convert.ToInt32(str);
+            int a_size = (int) Math.Pow(10, str.Length);
+            int i = 0;
+            string result = "";
+            while (a != 0 & i < 10)
+            {
+                i++;
+                a *= 2;
+                if (a > a_size)
+                {
+                    result = result + "1";
+                    a = a - a_size;
+                }
+                else
+                {
+                    result = result + "0";
+                }
+            }
+ 
+            return result;
+        }
+
+
+        // summary
+        // My Converter takes System.Double which is 52 bits (~15 decimals digits)
+        // and has a range of +/-5.0 x 10^-324 to +/-1.7 x 10^308
+        // converts it to string that is
+        // /summary
+        public static void MyConverter(double a)
+        {
+            Console.WriteLine(a);
+            string str = Convert.ToString(Math.Abs(a));
+            str.Replace(",", ".");
+            int point_pos = str.IndexOf('.');
+            int e_pos = (str.IndexOf('E') != -1) ? str.IndexOf('E') - 1 : str.Length - 1;
+            string str_0 = (a > 0) ? "0" : "1";
+            string str_1 = str.Substring(0, point_pos);
+            string str_2 = str.Substring(point_pos + 1, str.Length - 1 - point_pos - (str.Length - 1 - e_pos));
+            string str_3 = str.Substring(e_pos + 1, str.Length - 1 - e_pos);
+            Console.WriteLine(str_0 + " " + str_1 + " " + str_2 + " " + str_3);
+
+            string str_11 = decimal2binary(str_1);
+            Console.WriteLine("str_1: " + str_1 + "\nstr_11: " + str_11);
+
+            string str_21 = decimal2binary(Convert.ToString(127 + str_11.Length - 1));
+            Console.WriteLine(str_21);
+
+            string str_31 = decimal2binary1(str_2);
+
+
+
+            Console.WriteLine(str_0 +" " + str_21 + " " + str_11 + " " + str_31);
+
+
+        }
+
+        // summary
+        // My main
+        // /summary
+        private static void Main(string[] args)
+        {
+            MyConverter(263.3);//68.85);//
+            Console.WriteLine("--------------------------");
+            MyConverter(-255.255);
+            Console.WriteLine("--------------------------");
+          //  MyConverter(-263123456789123456789.3);
+        }
+    }
+}
---
+++ Task2_GCD/GCD_with_multiple_inputs/GCD_with_multiple_inputs/FindGCD.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+
+namespace GCD_with_multiple_inputs
+{
+    public class FindGCD
+    {
+        public static int binary_gcd(int a, int b)
+        {
+            if (a == 0) // if a=0 then answer is b
+                return b;
+            if (b == 0) // if b=0 then answer is a
+                return a;
+            if (a == 1 || b == 1) // if a or b equals 1 then asnwer is 1
+                return 1;
+            if (a == b) // if a equals b then answer is a
+                return a;
+            if (a % 2 == 0 && b % 2 == 0) // if both are even then we can take outside 2
+                return 2*binary_gcd(a / 2, b / 2);
+            if (a % 2 == 0)
+                return binary_gcd(a / 2, b); // if a is even then a divide by 2
+            if (b % 2 == 0)
+                return binary_gcd(a, b / 2); // if b is even then b divide by 2
+            if (a > b)
+                return binary_gcd((a - b) / 2, b); // if a>b then subtract and divide by 2
+            if (a < b)
+                return binary_gcd((b - a) / 2, a); // if a<b then subtract and divide by 2
+            return -1; // just in case something goes wrong
+        }
+
+        public static int MyBinaryGCD(params int[] a)
+        {
+
+            int temp = a[0]; // need a temporary storage
+
+            for (int i = 1; i < a.Length; i++) // loop through all elements
+            {
+                temp = binary_gcd(temp, a[i]); // find gcd one by one
+            }
+
+            return temp; // return final gcd
+        }
+
+        public static int gcd(int a, int b) // taken from https://habr.com/ru/post/205106
+        {
+            return (b == 0) ? Math.Abs(a) : gcd(b, a % b); // simply get gcd of two elements by recursively calling gcd and taking a remainder of the bigger 
+        }
+
+        public static int MyGCD(params int[] a)
+        {
+
+            int temp = a[0];
+
+            for (int i=1; i<a.Length; i++)
+            {
+                temp = gcd(temp, a[i]);
+            }
+            
+            return temp;
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Inputs are: 100, 50, 150");
+            Stopwatch sw = Stopwatch.StartNew();
+            Console.Write("multiple input GCD: {0}\t\t", MyGCD(100, 50, 150));
+            sw.Stop();
+            Console.WriteLine(" Time taken: {0}ms", sw.Elapsed.TotalMilliseconds);
+            sw = Stopwatch.StartNew();
+            Console.Write("multiple input binary GCD: {0}\t\t", MyBinaryGCD(100, 50, 150));
+            sw.Stop();
+            Console.WriteLine(" Time taken: {0}ms", sw.Elapsed.TotalMilliseconds);
+
+        }
+    }
+}
